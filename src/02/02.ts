c9d29dedc15f529912eee91d704d5ffead038fb8
hellpains <rustam2004sadulaev@mail.ru>
2022-11-29 00:52:09 +0300

    02 - JS/TS for students - объекты

---
+++ src/02/02.ts
@@ -0,0 +1,51 @@
+type LocalCityType ={
+    title: string
+    countryTitle: string
+}
+type AddressType={
+    streetTitle: string
+    city: LocalCityType
+}
+type TechnologiesType= {
+    id:number
+    title:string
+}
+
+
+type StudentType = {
+    name: string
+    age: number
+    isActive: boolean
+    address: AddressType
+    technologies: TechnologiesType[]
+}
+
+
+const student:StudentType = {
+    name: 'Rus',
+    age: 18,
+    isActive: false,
+    address: {
+        streetTitle: 'Komsomol',
+        city: {
+            title: 'Grozny',
+            countryTitle: 'Russia'
+        }
+    },
+    technologies: [
+        {
+            id: 1,
+            title: "HTML",
+        },
+        {
+            id: 2,
+            title: "CSS",
+        },
+        {
+            id: 3,
+            title: "React",
+        },
+    ]
+}
+
+console.log()
